bf7141d96e383dec833010895587c5903fb6f387
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-01-29 20:04:25 -0600

    Platforms sink!

--- platform.gd
+++ platform.gd
@@ -7,7 +7,10 @@ class_name Platform
 @onready var killer_gap_l: Area3D = %KillerGap_L
 @onready var killer_gap_r: Area3D = %KillerGap_R
 
+var animation: AnimationPlayer
+@export var meshes: Node3D
 
+@export var has_sinking: bool = false
 @export var has_two_killer_gaps: bool = false
 @export var uses_alt_killer_gap: bool = false
 
@@ -18,18 +21,33 @@ func _ready() -> void:
 	Messenger.state_msg_start.connect(_on_state_msg_start)
 	area_entered.connect(_on_area_entered)
 	
-	set_collision_layer_value(Globals.collision.PLATFORM_WRAP, true)
+	set_collision_layer_value(Globals.collision.PLATFORM_WRAP, true)	
+	set_collision_mask_value(Globals.collision.GROUND,false)
 	
 	if uses_alt_killer_gap:
 		change_killer_gap()
+		
+	if has_sinking:
+		setup_sinking()
 		
 	add_killer_gap()
 	
 func _on_area_entered(area):
 	#print("PLAT ",name," sees ",area)
 	if area.is_in_group("Player Area"):
-		pass
-		#print("PLAT ",name," sees Player!")
+		Messenger.remove_player.emit(true,Player.death_states.WATER)
+
+func setup_sinking():
+	animation = %AnimationPlayer
+	animation.play("sink")
+	
+func sink_removed_collision():
+	ground.set_collision_layer_value(Globals.collision.GROUND,false)
+	set_collision_mask_value(Globals.collision.GROUND,true)
+	
+func sink_added_collision():
+	ground.set_collision_layer_value(Globals.collision.GROUND,true)
+	set_collision_mask_value(Globals.collision.GROUND,false)
 
 func change_killer_gap():
 	killer_gap_l = %KillerGap_L2
@@ -45,6 +63,8 @@ func add_killer_gap():
 func _physics_process(delta: float) -> void:
 	if not direction == 0:
 		var rate: float = speed * delta
+
 		move_object(rate,direction,ground)
+		
 		self.global_position.x = ground.global_position.x
 	
